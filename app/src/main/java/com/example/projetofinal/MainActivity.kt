b6acb7f9d71eec7b0a7dd5e3752eae5c6b58ceb2
BrunoBorgesRey <brunoreysanchez@gmail.com>
2023-06-17 16:16:16 -0300

    Resolvendo o Problema de TextField

--- app/src/main/java/com/example/projetofinal/MainActivity.kt
+++ app/src/main/java/com/example/projetofinal/MainActivity.kt
@@ -6,40 +6,16 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -74,7 +50,7 @@ fun MenuPrincipal() {
         Button(
             onClick = {
                 Log.i("Teste","Botão Produto")
-
+                contexto.startActivity(Intent(contexto, TelaProduto::class.java))
             },
             modifier = Modifier.width(300.dp)) {
             Text(text = "Produtos")
@@ -84,7 +60,7 @@ fun MenuPrincipal() {
         Button(
             onClick = {
                 Log.i("Teste","Botão Pedidos")
-
+                contexto.startActivity(Intent(contexto, TelaPedido::class.java))
             },
             modifier = Modifier.width(300.dp)) {
             Text(text = "Pedidos")
--- app/src/main/java/com/example/projetofinal/TelaProduto.kt
+++ app/src/main/java/com/example/projetofinal/TelaProduto.kt
@@ -12,15 +12,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 class TelaProduto() : ComponentActivity()
@@ -28,7 +36,7 @@ class TelaProduto() : ComponentActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Log.i("Teste","Tela Cliente")
+            Log.i("Teste","Tela Produto")
             Produtos()
         }
     }
@@ -36,45 +44,93 @@ class TelaProduto() : ComponentActivity()
 
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun Produtos() {
     val contexto = LocalContext.current
-    val estadoCampoDeTextoCpf = remember { mutableStateOf(TextFieldValue()) }
-    val estadoCampoDeTexto = remember { mutableStateOf(TextFieldValue()) }
+    val estadoCampoDeTextoIdProduto = remember { mutableStateOf(TextFieldValue()) }
+    val estadoCampoDeTextoDescricao = remember { mutableStateOf(TextFieldValue()) }
+    val estadoCampoDeTextoValor = remember { mutableStateOf(TextFieldValue()) }
+    //val estadoCampoDeTextoFoto = remember { mutableStateOf(TextFieldValue()) }
     val activity = (LocalContext.current as? Activity)
     Column(
         Modifier.padding(40.dp)
     ) {
         Text(text="Tela de Produto", textAlign = TextAlign.Center,modifier = Modifier.fillMaxWidth())
         Spacer(modifier = Modifier.height(25.dp))
-        /*TextField(
-            value = estadoCampoDeTextoCpf.value,
-            onValueChange = { estadoCampoDeTextoCpf.value = it },
-            placeholder = { Text(text = "CPF") },
-            maxlines = 1,
-            modifier = Modifier.fillMaxWidth()
-
-        )*/
+        TextField(
+            value = estadoCampoDeTextoIdProduto.value,
+            onValueChange = {
+                estadoCampoDeTextoIdProduto.value = it
+            },
+            placeholder = { Text(text = "Insira o Id Produto") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,//Sem restrições (letras/números).
+                autoCorrect = true,
+            ),
+            textStyle = TextStyle(
+                color = Color.Black,
+                fontSize = TextUnit.Unspecified,
+                fontFamily = FontFamily.SansSerif
+            ),
+            maxLines = 1,
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = estadoCampoDeTextoDescricao.value,
+            onValueChange = {
+                estadoCampoDeTextoDescricao.value = it
+            },
+            placeholder = { Text(text = "Insira a Descrição") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,//Sem restrições (letras/números).
+                autoCorrect = true,
+            ),
+            textStyle = TextStyle(
+                color = Color.Black,
+                fontSize = TextUnit.Unspecified,
+                fontFamily = FontFamily.SansSerif
+            ),
+            maxLines = 1,
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = estadoCampoDeTextoValor.value,
+            onValueChange = {
+                estadoCampoDeTextoValor.value = it
+            },
+            placeholder = { Text(text = "Insira o Valor") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,//Sem restrições (letras/números).
+                autoCorrect = true,
+            ),
+            textStyle = TextStyle(
+                color = Color.Black,
+                fontSize = TextUnit.Unspecified,
+                fontFamily = FontFamily.SansSerif
+            ),
+            maxLines = 1,
+        )
         Spacer(modifier = Modifier.height(25.dp))
         Button(onClick = {
-            Log.i("TesteCliente","Botao Inserir")
+            Log.i("TesteProduto","Botao Inserir")
         }, modifier = Modifier.width(300.dp)) {
             Text(text = "Inserir")
         }
         Spacer(modifier = Modifier.height(25.dp))
         Button(onClick = {
-            Log.i("TesteCliente","Botao Inserir")
+            Log.i("TesteProduto","Botao Listar")
         }, modifier = Modifier.width(300.dp)) {
             Text(text = "Listar")
         }
         Spacer(modifier = Modifier.height(25.dp))
         Button(onClick = {
-            Log.i("TesteCliente","Botao Inserir")
+            Log.i("TesteProduto","Botao Deletar")
         }, modifier = Modifier.width(300.dp)) {
             Text(text = "Deletar")
         }
         Spacer(modifier = Modifier.height(25.dp))
         Button(onClick = {
-            Log.i("TesteCliente","Botao Inserir")
+            Log.i("TesteProduto","Botao Alterar")
         }, modifier = Modifier.width(300.dp)) {
             Text(text = "Alterar")
         }
