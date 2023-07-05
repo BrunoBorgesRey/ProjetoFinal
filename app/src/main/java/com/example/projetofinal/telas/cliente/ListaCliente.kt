794e000d6df666a40146012c175dafc1758254b1
BrunoBorgesRey <brunoreysanchez@gmail.com>
2023-07-05 14:23:33 -0300

    Lista Cliente

--- app/src/main/java/com/example/projetofinal/telas/cliente/ListaCliente.kt
+++ app/src/main/java/com/example/projetofinal/telas/cliente/ListaCliente.kt
@@ -4,26 +4,43 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DeleteForever
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LiveData
+import coil.compose.AsyncImage
 import com.example.projetofinal.models.Cliente
+import com.example.projetofinal.telas.produto.AlterarProduto
 import com.example.projetofinal.telas.produto.getProdutoRepository
 
 
@@ -59,46 +76,124 @@ fun ListaClientes(clientes: List<Cliente>) {
 fun ProductItem(cliente: Cliente) {
     val repository = getProdutoRepository()
     val contexto = LocalContext.current
-    Row(
-        modifier = Modifier
+
+    Card(
+        Modifier
+            .fillMaxHeight()
             .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(horizontal = 8.dp, vertical = 8.dp),
+        shape = RoundedCornerShape(8.dp)
     ) {
-        // Display the product details
-        Text(
-            text = cliente.nome,
-            style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
-            ),
-            modifier = Modifier.weight(1f)
-        )
-        Text(
-            text = cliente.cpf,
-            style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
-            ),
-            modifier = Modifier.weight(1f)
-        )
-        Button(
-            onClick = {
-                repository.removeCliente(cliente.id) },
-            modifier = Modifier.width(100.dp)
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
         ) {
-            Text(text = "Deletar")
-        }
-        Button(
-            onClick = {
-                val intent = Intent(contexto, AlterarCliente::class.java).apply {
-                    putExtra("myStringExtra", cliente.id)
+            Row(
+                Modifier
+                    .weight(9f)
+                    .fillMaxHeight()
+            ) {
+
+                Column(
+                    Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "CPF:${cliente.cpf}",
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        fontStyle = FontStyle.Normal,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                        ),
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Nome:${cliente.nome}", fontStyle = FontStyle.Normal,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                        ),
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text ="Telefone:${cliente.telefone}", fontStyle = FontStyle.Normal,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                        ),
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Endereco:${cliente.endereco}", fontStyle = FontStyle.Normal,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                        ),
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Instagram:${cliente.instagram}", fontStyle = FontStyle.Normal,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                        ),
+                        fontWeight = FontWeight.Bold,
+                    )
                 }
-                contexto.startActivity(intent)
-            },
-            modifier = Modifier.width(100.dp)
-        ) {
-            Text(text = "Alterar")
+            }
+
+
+            Column(
+                Modifier.width( 70.dp )
+            ) {
+                Button(
+                    onClick = {
+                        repository.removeCliente(cliente.id)
+                    },
+                    shape = RoundedCornerShape(100),
+                    modifier = Modifier.fillMaxSize()
+
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+
+                        Icon(
+                            Icons.Rounded.DeleteForever,
+                            contentDescription = null
+                        )
+
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = {
+                        val intent = Intent(contexto, AlterarCliente::class.java).apply {
+                            putExtra("myStringExtra", cliente.id)
+                        }
+                        contexto.startActivity(intent)
+                    },
+                    shape = RoundedCornerShape(100),
+                    modifier = Modifier
+                        .fillMaxSize()
+
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+
+                        Icon(
+                            Icons.Rounded.Edit,
+                            contentDescription = null
+                        )
+
+                    }
+
+                }
+            }
+
         }
     }
 }
